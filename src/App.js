cbd339e1dc484ec255289553cbe498051d7fa0ae
Kalen Martin <kalen.martin1@gmail.com>
2022-04-06 12:16:33 -0400

    first commit in new repo

---
+++ src/App.js
@@ -0,0 +1,44 @@
+// import React, {useState, useEffect } from "react";
+// import axios from "axios";
+// import he from "he";
+
+// const App = () => {
+//   const loginURL
+//   const signupURL
+//   const questionsURL
+//   const profileURL
+//   const [login, setLogin] = useState([]);
+//   const [registration, setRegistration] = useState([]);
+//   const [profile, setProfile] = useState([]);
+//   const [username, setUsername] = useState(null);
+//   const [home, setHome] = useState(true);
+//   const [askQuestion, setAskQuestion] = ([]);
+//   const [deleteQuestion, setDeleteQuestion] = ([]);
+//   const [questionList, setQuestionList] = ([]);
+//   const [questionCategories, setQuestionCategories] = ([]);
+//   const [answers, setAnswers] = ([]);
+//   const [correctAnswer, setCorrectAnswer] = (false);
+//   const [favorite, setFavorite] = (false);
+
+
+//   Registration
+//   useEffect(() => {
+//     axios
+//     .get(registrationURL)
+//     .then((response) => {
+//       setRegistration();
+//     }
+//     )
+//   }
+//   )
+
+
+
+
+//   return (
+//     <div>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ src/components/LoginReg.js
@@ -0,0 +1,18 @@
+// import React, { useState } from "react";
+// import he from "he";
+
+// const LoginReg = ({ login, registration, username, setUsername }) => {
+//     const [password, setPassword] = useState(false);
+
+//     const login = () => {
+//     }
+
+//     const registration = () => {
+//     }
+
+
+
+//     return ()
+// }
+
+// export default LoginReg
